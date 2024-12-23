e3f3528d94c9421057cf0d88d2c13f9565643921
Nishanth <nishanthkc009@gmail.com>
2024-12-23 08:08:17 +0530

    updated retriver
    add base code template

--- src/retriever.py
+++ src/retriever.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+class Retriever:
+
+    def __init__(self, location):
+        self._location = location
+
+    def _load_nested_space(self):
+        # TODO: load json file
+        pass
+
+    def get_experiment(self):
+        # TODO: return experiement details
+        pass
